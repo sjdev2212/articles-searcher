0cdabd61a0aad53791c8105dcb1e747752cbc70a
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-02-06 19:55:18 -0300

    set up views and func

---
+++ db/migrate/20230206214209_add_columns_to_articles.rb
@@ -0,0 +1,6 @@
+class AddColumnsToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :title, :string
+    add_column :articles, :body, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+articles = Article.create([{ title: "Change the world",
+                             body: "I graduated from high
+                             school, in Palo Alto,
+    the name Silicon Valley was not in use beyond
+    a small group of tech cognoscenti. Apple Computer
+    had incorporated the previous year, releasing the first
+     popular personal computer,
+    the Apple II. The major technology companies made
+    electronics
+    hardware, and on the way to school I rode my bike
+     through the
+    Stanford Industrial Park, past the offices of
+    Hewlett-Packard,
+    Varian, and Xerox PARC. The neighborhoods of the Santa Clara
+   Valley were dotted with cheap, modern, one-story
+   houses—called Eichlers,
+      after the builder Joseph Eichler—with glass walls,
+      open floor plans, and
+      flat-roofed carports. (Steve Jobs grew up in an
+        imitation Eichler, c
+      alled a Likeler.) The average house in Palo Alto
+       cost about a hundred and
+      twenty-five thousand dollars. Along the main downtown
+      street, University
+      Avenue—the future address of PayPal, Facebook, and
+      Google—were sports shops,
+      discount variety stores, and several art-house cinemas,
+       together with
+      the shuttered, X-rated Paris Theatre. Across El Camino Real,
+       the Stanford
+      Shopping Center was anchored by Macys and Woolworths,
+       with one boutique
+     store—a Victoria's Secret had opened in 1977—and
+     a parking lot
+      full of Datsuns and Chevy Novas. High-end dining
+       was virtually
+      unknown in Palo Alto, as was the adjective “high-end.”
+       The public
+      schools in the area were excellent and almost universally
+      attended;
+      the few kids I knew who went to private school had
+      somehow messed up.
+      The Valley was thoroughly middle class, egalitarian,
+       pleasant, and a
+              little boring." },
+                           { title: "The 23 and you",
+                             body: "CHERYL WHITTLE TRIED HER BEST to
+                             fall asleep, but her mind kept racing. Tomorrow
+                             was going to be the culmination of three years of research and, possibly,
+          a day that would change her life forever. Around four a.m.
+           she popped two Benadryl and managed to drift off. But in just a few
+            hours she had to be up and ready to go.
+          Cheryl and her husband, Dickie, are retired, and live in eastern
+           Virginia, way out on the end of the Northern Neck peninsula, which
+           juts like an arthritic finger into Chesapeake Bay.
+          It's a beautiful and isolated spot, where most people tack up
+          “No Trespassing” signs and stay close to home. The Whittles enjoy
+           their life in the country, but Cheryl was eager that day to make the long drive to meet Effie Jane.
+         She showered, threw on a T-shirt, jeans, and sneakers, dotted
+         make-up on her cheeks,
+          and scrunched a dollop of mousse into her thinning brown hair.
+        There's nothing showy about Cheryl, not even on a day like this.
+         She's short and shy, with nine grandchildren and no pretensions.
+         She grabbed a shoulder bag, heavy with the day's supplies, and
+         kissed Dickie on her way out the door." }])
