b039e1efcc46fcfee59617f8b5349454c5e96280
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-02-06 23:39:36 -0300

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  attr_accessible :email, :password, :password_confirmation
-  validates_uniqueness_of :email
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  resources :users
+  devise_for :users
+
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
